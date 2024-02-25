2e6450b0d1fac9b9e202ce63cb9cb5e0c79d255b
djkabutar <d.kabutarwala@yahoo.com>
2024-02-25 22:15:33 +0530

    hashfunc: change max hash value to int max
    Signed-off-by: djkabutar <d.kabutarwala@yahoo.com>

--- src/hashfunc.c
+++ src/hashfunc.c
@@ -2,13 +2,10 @@
  * This file contains the hash function used to hash the keys.
  * Function will return 32 bit hash value.
  */
-
-#include <math.h>
 #include <stdint.h>
 
 #include "cl2verilog.h"
 
-#define HASHSIZE (1 << 32)
 #define HASHSIZE16 (1 << 16)
 
 uint32_t hashfunc(const char *key, int len)
@@ -24,15 +21,15 @@ uint32_t hashfunc(const char *key, int len)
 	hash ^= (hash >> 11);
 	hash += (hash << 15);
 
-	return (hash % HASHSIZE);
+	return (hash % UINT32_MAX);
 }
 
 uint16_t hashfunc16(const char *key)
 {
-	int hash, i;
+	int hash;
 	for (int c = *key++; c != 0; c = *key++) {
 		hash = c + (hash << 6) + (hash << 16) - hash;
 	}
 
-	return (hash % HASHSIZE16);
+	return (hash % UINT16_MAX);
 }
